aef472875207f1ab3448183b20763f839c5b33a4
TolgaAkaMoon <4160141@RTS-365.dk>
2020-08-14 14:54:41 +0200

    done

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -18,14 +18,52 @@ navBarToggle.addEventListener("click", function() {
   }
 });
 
-fetch("http://localhost:4000/gallery-photos", {
+// Gallery
+fetch("http://localhost:4000/assets/", {
   "method": "GET"
 })
-.then(response => {console.log(response)
+.then(res => res.json())
+.then(assetId => {
+  fetch("http://localhost:4000/gallery-photos", {
+    "method": "GET"
+  })
+  .then(res => res.json())
+  .then(gallery => {
+  assetId.forEach( element=> {
 
-  
+        // Events
+    fetch("http://localhost:4000/events", {
+      "method": "GET"
+    })
+    .then(res => res.json())
+    .then(events => {
+      events.forEach(event => {
+        if (event.id == element.id) {
+          document.querySelector(".section__event").innerHTML += `
+          <img class="event__img" alt="events" src=${element.url}>
+          <div class="event__purple">
+              <p class="purple__text">${event.eventDate.slice(0, 10)}</p>
+              <p class="purple__text">00:00</p>
+              <p class="purple__text">${event.location}</p>
+          </div>
+          `
+        } else {
 
+        }
+      });
+    });
 
-})
-
-.catch(err => console.error(err));
+    gallery.forEach(Gasset => {
+      if (Gasset.asset == element.id) {
+        document.querySelector(".gallery").innerHTML += `
+        <figure class="gallery__figure">
+        <img class="gallery__img img1" src=${element.url} alt="club table">
+        <div class="gallery__triangle-top"></div>
+        <div class="gallery__triangle-bottom"></div>
+        </figure>
+        `}else {
+        }
+      })
+    });
+  });
+});
